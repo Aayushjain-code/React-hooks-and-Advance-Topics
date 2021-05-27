0ecfb7d9950b5ce1b4f48b1c721754d507a4dc75
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-27 19:03:32 +0530

    multiple states values in single function

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -8,14 +8,20 @@ const UseStateObject = () => {
     message: 'random Message'
   });
 
-  console.log(person);
+  const [name, setname] = useState('peter')
+  const [age, setage] = useState('24')
+  const [message, setmessage] = useState('random Message')
+
   const ChangeMessage = () => {
-    setPerson({ ...person, message: 'hello' })
+    // setPerson({ ...person, message: 'hello' });
+    setname('aayu');
+    setage('22')
+    setmessage("i am awesome")
   }
   return <>
-    <h3>{person.name}</h3>
-    <h3>{person.age}</h3>
-    <h3>{person.message}</h3>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
     <button className="btn" onClick={ChangeMessage}>ChangeMessage</button>
   </>;
 };
