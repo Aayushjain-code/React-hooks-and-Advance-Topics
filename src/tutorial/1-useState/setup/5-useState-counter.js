c0245c02537ffcf0809bbd05428ef2567296bf8d
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-27 19:17:17 +0530

    counter with only refrence function by calling

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,13 +2,23 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+
+  const increase = () => {
+    setValue(value + 1);
+  }
+  const decrease = () => {
+    setValue(value + 1);
+  }
+  const reset = () => {
+    setValue(0);
+  }
   return <>
     <section style={{ margin: '4rem 0' }}>
       <h2>regular counter</h2>
       <h1>{value}</h1>
-      <button className='btn' onClick={() => setValue(value - 1)}>Decrease</button>
-      <button className='btn' onClick={() => setValue(0)}>Reset</button>
-      <button className='btn' onClick={() => setValue(value + 1)}>Increase</button>
+      <button className='btn' onClick={decrease}>Decrease</button>
+      <button className='btn' onClick={reset}>Reset</button>
+      <button className='btn' onClick={increase}>Increase</button>
     </section>
   </>;
 };
