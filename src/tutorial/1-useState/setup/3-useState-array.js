4d918f7d849f9bf9e816ca2ec7b57c13291c9404
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-27 18:36:17 +0530

    objects in useState - spread operator

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -8,6 +8,7 @@ const UseStateArray = () => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople)
   }
+
   return <>
     {
       people.map((person) => {
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: '24',
+    message: 'random Message'
+  });
+
+  console.log(person);
+  const ChangeMessage = () => {
+    setPerson({ ...person, message: 'hello' })
+  }
+  return <>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button className="btn" onClick={ChangeMessage}>ChangeMessage</button>
+  </>;
 };
 
 export default UseStateObject;
