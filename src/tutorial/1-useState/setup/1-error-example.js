617e32526a58564bf7c72704e346ee2360fd46f8
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-27 10:14:39 +0530

    why we needed useState hook?(rendering)

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+
+  let title = 'random title';
+  const handleClick = () => {
+    title = "hello people";
+    console.log(title);
+
+  };
+  return (<div>
+    <h2>{title}</h2>
+    <button type="button" className="btn" onClick={handleClick}>Click Me</button>
+  </div>
+
+  );
 };
 
 export default ErrorExample;
+
+
+
+///here problem is that we are not re-rendering the title
+//when we see it in console log its seen but its not changing the display
+//here useState hook comes into play
