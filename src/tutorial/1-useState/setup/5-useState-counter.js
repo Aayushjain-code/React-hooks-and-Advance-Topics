abd50254b00ce6eb24c27a9c14c4deb16a2cd6b0
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-27 19:57:00 +0530

    Functionally updating a form by passing prev values

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
+
   const [value, setValue] = useState(0);
 
-  const increase = () => {
-    setValue(value + 1);
-  }
+
+
   const decrease = () => {
     setValue(value + 1);
   }
+
   const reset = () => {
     setValue(0);
   }
+
+  const complexIncrease = () => {
+    // setTimeout(()=>{ },2000)
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      })
+    }, 2000)
+  }
+
+
   return <>
     <section style={{ margin: '4rem 0' }}>
       <h2>regular counter</h2>
       <h1>{value}</h1>
       <button className='btn' onClick={decrease}>Decrease</button>
       <button className='btn' onClick={reset}>Reset</button>
-      <button className='btn' onClick={increase}>Increase</button>
+      <button className='btn' onClick={() => { setValue(value + 1) }}> Increase</button>
+    </section>
+
+
+    <section style={{ margin: '4rem 0' }}>
+      <h2>Complex counter</h2>
+      <h1>{value}</h1>
+
+      <button className='btn' onClick={complexIncrease}>Complex Increase</button>
     </section>
   </>;
 };
