bd18438540942a46a71da8f7eab50a02264b1fc1
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-28 22:31:54 +0530

    multiple returns-fetching basic example(imp)

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -6,17 +6,47 @@ const url = 'https://api.github.com/users/QuincyLarson';
 
 
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
 
 
-  if (loading) {
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json()
+        } else {
+          setIsLoading(false)
+          setIsError(true);
+          throw new Error(resp.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+        console.log(login);
+      })
+      .catch((error) => console.log(error));
+  }, [])
+
+
+
+  if (isLoading) {
     return <>
       <h2>loading...</h2>
+
+    </>
+  }
+  if (isError) {
+    return <>
+      <h2>Error...</h2>
     </>
   }
 
-
-  return <h2>multiple returns</h2>;
+  return <h2>{user}</h2>;
 
 };
 
