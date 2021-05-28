b55ad010711a428a2756d2ef222b064d86b69885
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-28 18:49:48 +0530

    dependency list,second parameter

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
   useEffect(() => {
-    if (value > 5) {
+    if (value > 1) {
       console.log('call useEffect');
       document.title = `new messages(${value})`;
     }
 
-  });
+  }, [value]);
 
+  useEffect(() => {
+    console.log("hello");
+  })
   console.log('render component');
+
   return <>
     <h2>{value}</h2>
     <button className='btn' onClick={() => setValue(value + 1)}> click me</button>
